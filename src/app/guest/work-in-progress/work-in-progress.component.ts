ac8797b5c88f71511bb673325028ff1d270d7012
DeemayJ <m.dimaggio124@gmail.com>
2019-11-11 16:58:04 +0100

    JSON fix

--- src/app/guest/work-in-progress/work-in-progress.component.ts
+++ src/app/guest/work-in-progress/work-in-progress.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { UserDTO } from 'src/dto/userdto';
 
 @Component({
   selector: 'app-work-in-progress',
@@ -7,9 +8,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class WorkInProgressComponent implements OnInit {
 
+  user: UserDTO;
   constructor() { }
 
   ngOnInit() {
+    this.user=JSON.parse(localStorage.getItem('currentUser'));
   }
 
 }
